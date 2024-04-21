1c44b0ba5b1ac11d28341abf79d1ad3f786f5dbf
hitak <hitak77@naver.com>
2024-04-21 19:16:54 +0900

    정수기

--- app/src/main/java/com/tak/waterapp/MainActivity.kt
+++ app/src/main/java/com/tak/waterapp/MainActivity.kt
@@ -2,8 +2,11 @@ package com.tak.waterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tak.waterapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding = ActivityMainBinding.inflate(layoutInflater)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
